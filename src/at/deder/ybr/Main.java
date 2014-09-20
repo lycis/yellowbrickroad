25cfc5c18bd3fd6b582a4caa69523abc7e11a2e9
Daniel Eder <daniel@deder.at>
2014-09-20 13:44:02 +0200

    basic setup

---
+++ src/at/deder/ybr/Main.java
@@ -0,0 +1,17 @@
+package at.deder.ybr;
+
+/**
+ * This class is the main entry point for the application. It parses all
+ * command line parameters and triggers according actions.
+ * 
+ * @author lycis
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
