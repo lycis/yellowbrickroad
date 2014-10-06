532787d757e5de77d91701a8b404b2325a6f70e9
Daniel Eder <daniel@deder.at>
2014-10-06 16:40:42 +0200

    #3 refactorings for file output channel

--- src/at/deder/ybr/access/FileOutputChannel.java
+++ src/at/deder/ybr/access/FileOutputChannel.java
@@ -7,40 +7,48 @@ import java.io.IOException;
 
 /**
  * Grants access to logfile writer.
+ *
  * @author lycis
  */
 public class FileOutputChannel implements IOutputChannel {
 
-    private File outputFile             = null;
-    private BufferedWriter outputWriter = null;
-    
+    private File outputFile = null;
+
     public FileOutputChannel(File f, boolean append) throws IOException {
         outputFile = f;
+
+        // clean file if append is not wanted
+        if (!append) {
+            cleanFile(f);
+        }
+
     }
-    
+
     public FileOutputChannel(File f) throws IOException {
         this(f, false);
     }
-        
+
     @Override
     public void print(String s) {
+        BufferedWriter outputWriter = null;
         try {
             outputWriter = new BufferedWriter(new FileWriter(outputFile, true));
             outputWriter.write(s);
         } catch (IOException ex) {
-            System.err.println("exception: "+ex.getMessage());
-        }finally {
-            if(outputWriter != null) {
-                try{
+            System.err.println("error while writing to log: " + ex.getMessage());
+        } finally {
+            if (outputWriter != null) {
+                try {
                     outputWriter.close();
-                } catch (IOException ex) {} 
+                } catch (IOException ex) {
+                }
             }
         }
     }
 
     @Override
     public void println(String s) {
-        print(s+"\n");
+        print(s + "\n");
     }
 
     @Override
@@ -50,7 +58,31 @@ public class FileOutputChannel implements IOutputChannel {
 
     @Override
     public void printErrLn(String s) {
-        printErr(s+"\n");
+        printErr(s + "\n");
     }
-    
+
+    /**
+     * Clear all content from a file.
+     * @param f
+     * @throws IOException 
+     */
+    private void cleanFile(File f) throws IOException{
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(f));
+            writer.write("");
+            writer.close();
+        } catch (IOException ex) {
+            throw ex;
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException ex) {
+                    // do nothing
+                }
+            }
+        }
+    }
+
 }
