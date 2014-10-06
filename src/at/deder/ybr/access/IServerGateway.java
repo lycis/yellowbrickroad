5d72a21802070f626200349776cc2ed10026e15b
Daniel Eder <daniel@deder.at>
2014-10-06 18:28:57 +0200

    #5 server gateway interface

---
+++ src/at/deder/ybr/access/IServerGateway.java
@@ -0,0 +1,20 @@
+package at.deder.ybr.access;
+
+import at.deder.ybr.beans.ServerManifest;
+
+/**
+ * This interface represents a gateway to a remote YBR server. The complete
+ * implementation has to be designed stateless (or at least that is what this
+ * interface aimes for). The actual protocol is hidden from a user of this
+ * interface so that abstract access is possible.
+ * 
+ * @author lycis
+ */
+public interface IServerGateway {
+    
+    /**
+     * Provides the manifest of the server.
+     * @return manifest bean
+     */
+    public ServerManifest getManifest();
+}
