e7abbf32ae836bd908b7a7f68d5fea18dfaaf8c4
Daniel Eder <daniel@deder.at>
2014-11-14 14:49:23 +0100

    skeleton for update command

---
+++ src/at/deder/ybr/commands/Update.java
@@ -0,0 +1,26 @@
+package at.deder.ybr.commands;
+
+import java.util.List;
+
+/**
+ * Fetches all packages according to the config file.
+ * @author lycis
+ */
+public class Update implements ICliCommand {
+
+    @Override
+    public void setOption(String name, String value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setData(List<String> cliData) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void execute() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
