5cda0e1ecef82e0c5968c062894cec0c4c657a39
Daniel Eder <daniel@deder.at>
2014-11-20 10:26:45 +0100

    implemented first steps

--- test/at/deder/ybr/test/cukes/steps/CukeSteps.java
+++ test/at/deder/ybr/test/cukes/steps/CukeSteps.java
@@ -1,9 +1,16 @@
 package at.deder.ybr.test.cukes.steps;
 
-import cucumber.api.PendingException;
+import at.deder.ybr.channels.OutputChannelFactory;
+import at.deder.ybr.commands.Update;
+import at.deder.ybr.filesystem.FileSystem;
+import at.deder.ybr.test.mocks.CheckableSilentOutputChannel;
+import at.deder.ybr.test.mocks.MockFileSystemAccessor;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import java.io.File;
+import java.util.ArrayList;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Steps for Cucumber tests.
@@ -11,22 +18,34 @@ import cucumber.api.java.en.When;
  * @author lycis
  */
 public class CukeSteps {
-
+    MockFileSystemAccessor filesystem   = new MockFileSystemAccessor();
+    CheckableSilentOutputChannel output = new CheckableSilentOutputChannel();
+    
+    public CukeSteps() {
+        // set default accessors and channels
+        FileSystem.injectAccessor(filesystem);
+        OutputChannelFactory.setOutputChannel(output);
+    }
+    
     @Given("^there is no config file")
     public void there_is_no_config_file() throws Throwable {
-        throw new PendingException();
+        File configFile = filesystem.getClientConfigFile(".");
+        if(configFile != null) {
+            configFile.delete();
+        }
     }
 
     @When("^I update the project$")
     public void i_update_the_project() throws Throwable {
-        // Write code here that turns the phrase above into concrete acts
-        throw new PendingException();
+        Update cmd = new Update();
+        cmd.setData(new ArrayList<String>()); // no args
+        cmd.execute();
+        
     }
 
     @Then("^the error \"(.*?)\" is displayed$")
-    public void the_error_is_displayed(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete acts
-        throw new PendingException();
+    public void the_error_is_displayed(String error) throws Throwable {
+        assertThat(output.getError()).isEqualTo("error: "+error);
     }
 
 }
