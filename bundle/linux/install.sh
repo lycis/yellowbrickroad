ba62cfd43c0198cfbed0365e3c5a18f8b94e559b
Daniel Eder <daniel@deder.at>
2014-12-20 10:47:18 +0100

    Fixed start script for linux
    	modified:   bundle/linux/install.sh

--- bundle/linux/install.sh
+++ bundle/linux/install.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-#
-# This script installs ybr on your system.
-#
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
-install -d /usr/lib/yellow-brick-road/
-
-for f in ./lib/*; do \
-    install -D -t /usr/lib/yellow-brick-road/ $f; \
-done
-
-for f in ./bin/*; do \
-    install -D -t /usr/bin/ $f; \
-done
+java -cp "/usr/lib/yellow-brick-road/*" at.deder.ybr.Main $@
