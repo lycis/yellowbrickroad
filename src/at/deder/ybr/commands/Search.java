879fe7313434e568a0926be965d89e7ef5a4c0eb
Daniel Eder <daniel@deder.at>
2014-12-27 09:08:26 +0100

    skeleton for "search" command

---
+++ src/at/deder/ybr/commands/Search.java
@@ -0,0 +1,26 @@
+package at.deder.ybr.commands;
+
+import java.util.List;
+
+/**
+ * Search and list packages by name.
+ * @author lycis
+ */
+public class Search implements ICliCommand{
+
+    @Override
+    public void setOption(String name, String value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setData(List<String> cliData) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void execute() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
